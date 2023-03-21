b51914841d7f6c04e48d052d625e1855a00d3415
jae woo Jung <awfjol2008@naver.com>
2023-03-22 00:57:45 +0900

    OrderSearch.java

--- src/main/java/jpabook/jpashop/repository/OrderSearch.java
+++ src/main/java/jpabook/jpashop/repository/OrderSearch.java
@@ -2,11 +2,7 @@ package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.OrderStatus;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import org.springframework.stereotype.Repository;
-
-import javax.persistence.EntityManager;
 
 @Getter @Setter
 public class OrderSearch {
