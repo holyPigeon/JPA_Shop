d1d0e933350117c0d63179c591fb3e0fe7bf0e20
jae woo Jung <awfjol2008@naver.com>
2023-03-27 23:23:41 +0900

    UpdateItemDTO.java

--- src/main/java/jpabook/jpashop/service/UpdateItemDTO.java
+++ src/main/java/jpabook/jpashop/service/UpdateItemDTO.java
@@ -8,4 +8,10 @@ public class UpdateItemDTO {
   private String name;
   private int price;
   private int stockQuantity;
+
+  public UpdateItemDTO(String name, int price, int stockQuantity) {
+    this.name = name;
+    this.price = price;
+    this.stockQuantity = stockQuantity;
+  }
 }
